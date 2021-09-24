fc1be98837788f4c825c248499f4aee727236bcc
Joel Michelson <joelmichelson@users.noreply.github.com>
2021-09-24 06:15:00 -0500

    Add files via upload

--- marlgrid/envs/yummyyucky.py
+++ marlgrid/envs/yummyyucky.py
@@ -3,7 +3,35 @@ from ..objects import *
 import random
 import math
 
-class YummyYuckyEnv(MultiGridEnv):
+class YummyYuckyEnv0(MultiGridEnv):
+    """
+    """
+
+    mission = "yummy yucky simple: go to the correct color, of 2."
+    metadata = {}
+
+    def _gen_grid(self, width, height):
+        # Create an empty grid
+        self.grid = MultiGrid((width, height))
+
+        chosen = 0 # choose green as the good color
+        mirror1 = random.choice([-1,1])
+
+        c = ['green', 'blue']
+
+        # Generate the surrounding walls
+        self.grid.wall_rect(0, 0, width, height)
+
+        for x in range(2):
+            r = 1 if x == chosen else -1
+            self.put_obj(Goal(color=c[x], reward=r), width//2 + 1*(x*2-1)*mirror1, height//2)
+
+
+        self.agent_spawn_kwargs = {"top":(1,1)}
+        self.place_agents(**self.agent_spawn_kwargs)
+
+
+class YummyYuckyEnv1(MultiGridEnv):
     """
     """
 
