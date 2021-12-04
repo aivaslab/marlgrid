4d01b1f057625e3d8f3d12c29917e3cc06019c3f
Joel Michelson <joelmichelson@users.noreply.github.com>
2021-12-04 15:07:14 -0600

    Update base_AEC.py

--- marlgrid/base_AEC.py
+++ marlgrid/base_AEC.py
@@ -443,12 +443,10 @@ class para_MultiGridEnv(ParallelEnv):
     def observation_space(self, name):
         return self.observation_spaces[name]
 
-    @property
     def action_space(self):
         return self.action_spaces[self.possible_agents[0]]
 
-    @property
-    def observation_space(self, name):
+    def observation_space(self):
         return self.observation_spaces[self.possible_agents[0]]
 
     def seed(self, seed=1337):
