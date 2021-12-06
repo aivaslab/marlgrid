b6684de27423aa52efe3588d14532fe17e10c346
Joel Michelson <joelmichelson@users.noreply.github.com>
2021-12-05 18:25:58 -0600

    Update __init__.py

--- marlgrid/pz_envs/__init__.py
+++ marlgrid/pz_envs/__init__.py
@@ -3,3 +3,10 @@ from ..objects import *
 from random import randrange
 import random
 import math
+
+from .compfeed import *
+from .contentFB import *
+from .doorkey import *
+from .empty import *
+from .knowguess import *
+from .yummyyucky import *
