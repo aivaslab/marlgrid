7ee7e1e1b4df00d6c8c6afe9389dd24a8221906f
Joel Michelson <joelmichelson@users.noreply.github.com>
2021-12-15 14:28:30 -0600

    add carrying display

--- marlgrid/objects.py
+++ marlgrid/objects.py
@@ -159,6 +159,10 @@ class GridAgent(WorldObj):
 		tri_fn = rotate_fn(tri_fn, cx=0.5, cy=0.5, theta=0.5 * np.pi * (self.dir))
 		fill_coords(img, tri_fn, COLORS[self.color])
 
+		if agent.carrying is not None:
+			agent.carrying.render(img)
+
+
 
 class BulkObj(WorldObj, metaclass=RegisteredObjectType):
 	# Todo: special behavior for hash, eq if the object has an agent.
