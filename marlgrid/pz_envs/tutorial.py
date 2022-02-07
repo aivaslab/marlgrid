e754dbfcf800bad7072197a4ea64606a44902160
Joel Michelson <joelmichelson@users.noreply.github.com>
2022-02-07 13:52:31 -0600

    fix tutorial stage 2 doors being broken

--- marlgrid/pz_envs/tutorial.py
+++ marlgrid/pz_envs/tutorial.py
@@ -13,8 +13,8 @@ class para_TutorialEnv(para_MultiGridEnv):
     mission = "get to the goal"
     metadata = {'render.modes': ['human', 'rgb_array'], "name": "tutorial"}
     random_mode = False
-    mylevel = 1
-    mysublevel = 1
+    mylevel = 2
+    mysublevel = 6
 
     def _rand_int(self, x, y):
         return randrange(x, y)
@@ -54,18 +54,18 @@ class para_TutorialEnv(para_MultiGridEnv):
                             if num == 0:
                                 self.put_obj(Wall(), x, y)
                             elif num == 1:
-                                self.put_obj(Door(color=colors[0], state=randrange(0,3)), x, y)
+                                self.put_obj(Door(color=colors[0], state=randrange(1,4)), x, y)
                             elif num == 2:
-                                self.put_obj(Key(color=colors[0], state=randrange(0,3)), x, y)
+                                self.put_obj(Key(color=colors[0], state=randrange(1,4)), x, y)
             if sublevel == 4:
                 for i in range(3):
                     num = randrange(0,3)
                     if num == 0:
                         self.place_obj(Wall(), top=(0, 0), size=(width, height))
                     elif num == 1:
-                        self.place_obj(Door(color=colors[0], state=randrange(0,3)), top=(0, 0), size=(width, height))
+                        self.place_obj(Door(color=colors[0], state=randrange(1,4)), top=(0, 0), size=(width, height))
                     elif num == 2:
-                        self.place_obj(Key(color=colors[0], state=randrange(0,3)), top=(0, 0), size=(width, height))
+                        self.place_obj(Key(color=colors[0], state=randrange(1,4)), top=(0, 0), size=(width, height))
 
             self.place_obj(Goal(color="green", reward=100), top=(0, 0), size=(width, height))
 
@@ -77,24 +77,24 @@ class para_TutorialEnv(para_MultiGridEnv):
             doorIdx = self._rand_int(1, height - 2)
             
             if sublevel in [1,3]:
-                self.put_obj(Door(color=colors[0], state=0), splitIdx, doorIdx)
-            if sublevel == 2:
                 self.put_obj(Door(color=colors[0], state=1), splitIdx, doorIdx)
-            if sublevel in [4,5,6]:
+            if sublevel == 2:
                 self.put_obj(Door(color=colors[0], state=2), splitIdx, doorIdx)
+            if sublevel in [4,5,6]:
+                self.put_obj(Door(color=colors[0], state=3), splitIdx, doorIdx)
             if sublevel in [3,4]:
-                self.put_obj(Key(colors[0]), splitIdx-1, doorIdx)
+                self.put_obj(Key(color=colors[0]), splitIdx-1, doorIdx)
             if sublevel in [5,6]:
-                self.place_obj(Key(colors[0]), top=(1, 1), size=(splitIdx, height-1))
+                self.place_obj(Key(color=colors[0]), top=(1, 1), size=(splitIdx, height-1))
             if sublevel == 6:
-                self.place_obj(Key(colors[1]), top=(1, 1), size=(splitIdx, height-1))
+                self.place_obj(Key(color=colors[1]), top=(1, 1), size=(splitIdx, height-1))
 
             self.put_obj(Goal(color="green", reward=100), width - 2, height//2)
 
             self.agent_spawn_kwargs = {'size': (splitIdx, height)}
 
 
-        elif level == 3: #preferential doorkey variants
+        elif level == 3: #4-way doorkey variants
             goals = random.sample([0,0,0,1], 4)
 
             goal_positions = [(width-2,height//2), (width//2,1), (1,height//2), (width//2, height-2)]
@@ -114,10 +114,10 @@ class para_TutorialEnv(para_MultiGridEnv):
             for color, goal, goal_p, door, sub in zip(colors, goals, goal_positions, door_positions, subgoal_positions):
                 
                 if sublevel == 1:
-                    self.put_obj(Door(color=color, state=1), door[0], door[1])
-                elif sublevel == 2:
                     self.put_obj(Door(color=color, state=2), door[0], door[1])
-                    self.place_obj(obj=Key(color), top=(3, 3), size=(width-6, height-6))
+                elif sublevel == 2:
+                    self.put_obj(Door(color=color, state=3), door[0], door[1])
+                    self.place_obj(obj=Key(color=color), top=(3, 3), size=(width-6, height-6))
 
                 self.put_obj(Goal(color='green', reward=50+50*goal), goal_p[0], goal_p[1])
 
