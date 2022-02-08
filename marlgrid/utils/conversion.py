d64d383cb9c817aeb3a8dffc5c5538fec3a5cf26
Joel Michelson <joelmichelson@users.noreply.github.com>
2022-02-07 20:29:09 -0600

    Update conversion.py

--- marlgrid/utils/conversion.py
+++ marlgrid/utils/conversion.py
@@ -1,4 +1,4 @@
-from pettingzoo.utils.conversions import to_parallel, from_parallel
+from pettingzoo.utils.conversions import aec_to_parallel, from_parallel
 import supersuit as ss
 from pettingzoo.utils import wrappers
 
@@ -30,7 +30,7 @@ def pz2sb3(env, num_cpus=2):
     takes a wrapped env 
     returns sb3 compatible via supersuit concatenated set of vector environments
     '''
-    envInstance = to_parallel(env)
+    envInstance = aec_to_parallel(env)
     env2 = ss.black_death_v2(ss.pad_action_space_v0(ss.pad_observations_v0(envInstance)))
     env2 = ss.pettingzoo_env_to_vec_env_v1(env2)
     env2 = ss.concat_vec_envs_v1(env2, num_cpus, base_class='stable_baselines3')
