a699a498f6710b538dbbb9a6a3ab7407a7b2a882
Joel Michelson <joelmichelson@users.noreply.github.com>
2022-02-17 13:20:26 -0600

    stage convention

--- marlgrid/pz_envs/tutorial.py
+++ marlgrid/pz_envs/tutorial.py
@@ -12,9 +12,9 @@ class para_TutorialEnv(para_MultiGridEnv):
 
     mission = "get to the goal"
     metadata = {'render.modes': ['human', 'rgb_array'], "name": "tutorial"}
-    random_mode = False
-    mylevel = 2
-    mysublevel = 1
+    random_mode = True
+    stages = ["1a", "1b", "1c", "1d", "2a", "2b", "2c", "2d", "2e", "2f", "3a", "3b"]
+    curStage = 0;
     loading_mode = False
     saving_mode = False
     path = ''
@@ -25,29 +25,23 @@ class para_TutorialEnv(para_MultiGridEnv):
     def _gen_grid(self, width, height):
         self.grid = MultiGrid((width, height))
         if self.random_mode:
-            level = randrange(1,4)
-            if level == 1:
-                sublevel = randrange(1,5)
-            elif level == 2:
-                sublevel = randrange(1,7)
-            elif level == 3:
-                sublevel = randrange(1,3)
+            stage = random.choice(self.stages)
         else:
-            level = self.mylevel
-            sublevel = self.mysublevel
+            stage = self.stages[self.curStage]
+            self.curStage = (self.curStage + 1) % len(self.stages) 
 
         colors = random.sample(['purple','orange','yellow','blue','pink','red'], 4)
 
         #grid and surrounding walls
         self.grid.wall_rect(0, 0, width, height)
 
-        if level == 1: #empty, cluttered, and mazes
+        if "1" in stage: #empty, cluttered, and mazes
             #self.put_obj(Goal(color="green", reward=100), width - 2, height//2)
 
-            if sublevel in [2,3,4]:
+            if stage[1] in "bcd":
                 for x in range(2, width-1, 2):
                     for y in range(2, height-1, 2):
-                        if sublevel in [3,4]:
+                        if stage[1] in "cd":
                             self.put_obj(Wall(), x, y)
                         else:
                             num = randrange(0,10)
@@ -57,7 +51,7 @@ class para_TutorialEnv(para_MultiGridEnv):
                                 self.put_obj(Door(color=colors[0], state=randrange(1,4)), x, y)
                             elif num == 2:
                                 self.put_obj(Key(color=colors[0], state=randrange(1,4)), x, y)
-            if sublevel == 4:
+            if stage[1] == "d":
                 for i in range(3):
                     num = randrange(0,3)
                     if num == 0:
@@ -70,23 +64,23 @@ class para_TutorialEnv(para_MultiGridEnv):
             self.place_obj(Goal(color="green", reward=100), top=(0, 0), size=(width, height))
 
 
-        elif level == 2: #doorkey variants
+        elif "2" in stage: #doorkey variants
             splitIdx = self._rand_int(2, width - 2)
             self.grid.vert_wall(splitIdx, 1)
 
             doorIdx = self._rand_int(1, height - 2)
             
-            if sublevel in [1,3]:
+            if stage[1] in "ac":
                 self.put_obj(Door(color=colors[0], state=1), splitIdx, doorIdx)
-            if sublevel == 2:
+            if stage[1] == "b":
                 self.put_obj(Door(color=colors[0], state=2), splitIdx, doorIdx)
-            if sublevel in [4,5,6]:
+            if stage[1] in "def":
                 self.put_obj(Door(color=colors[0], state=3), splitIdx, doorIdx)
-            if sublevel in [3,4]:
+            if stage[1] in "cd":
                 self.put_obj(Key(color=colors[0]), splitIdx-1, doorIdx)
-            if sublevel in [5,6]:
+            if stage[1] in "ef":
                 self.place_obj(Key(color=colors[0]), top=(1, 1), size=(splitIdx, height-1))
-            if sublevel == 6:
+            if stage[1] == "f":
                 self.place_obj(Key(color=colors[1]), top=(1, 1), size=(splitIdx, height-1))
 
             self.put_obj(Goal(color="green", reward=100), width - 2, height//2)
@@ -94,7 +88,7 @@ class para_TutorialEnv(para_MultiGridEnv):
             self.agent_spawn_kwargs = {'size': (splitIdx, height)}
 
 
-        elif level == 3: #4-way doorkey variants
+        elif "3" in stage: #4-way doorkey variants
             goals = random.sample([0,0,0,1], 4)
 
             goal_positions = [(width-2,height//2), (width//2,1), (1,height//2), (width//2, height-2)]
@@ -113,9 +107,9 @@ class para_TutorialEnv(para_MultiGridEnv):
             # Place doors, goals, subgoals
             for color, goal, goal_p, door, sub in zip(colors, goals, goal_positions, door_positions, subgoal_positions):
                 
-                if sublevel == 1:
+                if stage[1] == "a":
                     self.put_obj(Door(color=color, state=2), door[0], door[1])
-                elif sublevel == 2:
+                elif stage[1] == "b":
                     self.put_obj(Door(color=color, state=3), door[0], door[1])
                     self.place_obj(obj=Key(color=color), top=(3, 3), size=(width-6, height-6))
 
