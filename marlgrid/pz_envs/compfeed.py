be063b80583d899bd3ce0a7f5e618a3afa6f7c6f
Joel Michelson <joelmichelson@users.noreply.github.com>
2022-03-03 13:26:00 -0600

    Add files via upload

--- marlgrid/pz_envs/compfeed.py
+++ marlgrid/pz_envs/compfeed.py
@@ -13,10 +13,11 @@ class para_CompFeedEnv(para_MultiGridEnv):
     def _gen_grid(self, width, height):
         width = 15
         height = 9
-        curType = "informed" #both glass, food baited, both released
+        #curType = "informed" #both glass, food baited, both released
         #curType = "uninformed" #both glass, dom blinded, food baited, both released
         #curType = "control misinformed" #both glass, food baited, food swapped, both released
         #curType = "misinformed" #both glass, food baited, dom blinded, food swapped, both released
+        curType = random.choice(["informed","uninformed","control misinformed","misinformed"])
 
         self.timers = {}
 
@@ -27,12 +28,12 @@ class para_CompFeedEnv(para_MultiGridEnv):
             self.timers["dBlind"] = 2
         if curType == "misinformed":
             self.timers["dBlind"] = 6
-            self.timers["dSwitch"] = 7
+            self.timers["swap"] = 7
             self.timers["hide"] = 8
             self.timers["release"] = 9
         if curType == "control misinformed":
             #self.timers["dBlind"] = (6)
-            self.timers["dSwitch"] = 7
+            self.timers["swap"] = 7
             self.timers["hide"] = 8
             self.timers["release"] = 9
 
@@ -58,19 +59,38 @@ class para_CompFeedEnv(para_MultiGridEnv):
 
 
         #puppet guiding arrow
-        direction = random.choice([1,3])
-        if "mis" not in curType:
+        
+        if curType == "informed" or curType == "misinformed":
             direction = 1+self.food_loc*2
-        self.put_obj(Arrow(color='red', direction=direction), 6, 4)
+        elif curType == "control misinformed":
+            direction = 3-(self.food_loc)*2
+        else:
+            direction = random.choice([1,3])
+        self.put_obj(Arrow(direction=direction), 8, 4)
 
         for x in range(7,9):
             for y in range(1,4):
                 self.put_obj(Lava(), x, y)
                 self.put_obj(Lava(), x, y+4)
+                
+        self.put_obj(Lava(), 7, 4)
+        self.del_obj(8,3)
+        self.del_obj(8,2)
+        self.del_obj(7,2)
+        self.del_obj(8,5)
+        self.del_obj(8,6)
+        self.del_obj(7,6)
+        self.put_obj(Arrow(direction=0), 8, 2)
+        self.put_obj(Arrow(direction=0), 8, 6)
 
         self.agent_spawn_kwargs = {'top': (0,3), 'size': (2, 2)}
-        self.agent_spawn_pos = {'player_0': (1,4), 'player_1': (10, 4)}
+        self.agent_spawn_pos = {'player_0': (1,4,0), 'player_1': (10, 4, 2)}
         #todo: place puppet agent too
+        '''for agent in self.agent_instances:
+        	if agent.name == 'player_1':
+        		agent.dir = 2
+        	if agent'''
+        print([x.dir for x in self.agent_instances])
 
         #self.place_agents(**self.agent_spawn_kwargs)
 
@@ -94,5 +114,21 @@ class para_CompFeedEnv(para_MultiGridEnv):
             self.put_obj(b1, 6, 2)
             self.put_obj(b2, 6, 6)
         elif name == "release":
-            self.del_obj(2, 4)
+            self.del_obj(2, 4) #should change to transparant mode?
             self.del_obj(9, 4)
+        elif name == "dBlind":
+            self.put_obj(Block(color='red', init_state=1), 9, 4) 
+        if name == "swap":
+            # swap foods and arrow
+            if self.food_loc == 0: #6, 2 and 6, 6
+                self.put_obj(Goal(color='green', reward=100), 6, 6)
+                self.put_obj(Goal(color='green', reward=50, size=0.5), 6, 2)
+            else:
+                self.put_obj(Goal(color='green', reward=100), 6, 2)
+                self.put_obj(Goal(color='green', reward=50, size=0.5), 6, 6)
+            
+            
+            #todo: add intro to food hiding in boxes
+            #right after food of different sizes
+            
+            #add control scenario where block goes solid and then no swap happens
