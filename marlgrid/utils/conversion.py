3d7b0b88fe4518f297d3c837f06e776024960b56
Joel Michelson <joelmichelson@users.noreply.github.com>
2022-03-26 14:23:25 -0500

    black death update

--- marlgrid/utils/conversion.py
+++ marlgrid/utils/conversion.py
@@ -31,7 +31,7 @@ def pz2sb3(env, num_cpus=2):
     returns sb3 compatible via supersuit concatenated set of vector environments
     '''
     envInstance = aec_to_parallel(env)
-    env2 = ss.black_death_v2(ss.pad_action_space_v0(ss.pad_observations_v0(envInstance)))
+    env2 = ss.black_death_v3(ss.pad_action_space_v0(ss.pad_observations_v0(envInstance)))
     env2 = ss.pettingzoo_env_to_vec_env_v1(env2)
     env2 = ss.concat_vec_envs_v1(env2, num_cpus, base_class='stable_baselines3')
     env2.black_death = True
