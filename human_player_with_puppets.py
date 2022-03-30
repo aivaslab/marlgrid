f84e374ff26c634d25793ffa1cafe5d617205e2a
Joel Michelson <joelmichelson@users.noreply.github.com>
2022-03-29 20:15:49 -0500

    Add files via upload

--- human_player_with_puppets.py
+++ human_player_with_puppets.py
@@ -37,7 +37,7 @@ class HumanPlayer:
 
 
 env_config =  {
-    "env_class": "para_Mindreading2",
+    "env_class": "para_Mindreading",
     "max_steps": 50,
     "respawn": True,
     "ghost_mode": True,
@@ -57,9 +57,7 @@ player_interface_config = {
 }
 
 
-env_config['agents'] = [GridAgentInterface(**player_interface_config)]
-
-env_config['puppets'] =  [GridAgentInterface(**player_interface_config)]
+env_config['agents'] = [GridAgentInterface(**player_interface_config), GridAgentInterface(**player_interface_config)]
 
 env = env_from_config(env_config)
 
@@ -75,15 +73,46 @@ for i in range(5):
     env.hard_reset(scenario_configs[config])
     print(env.params)
     obs = env.reset()
+    nextActs = []
+    pathDict = {}
     while True:
 
         #env.unwrapped.render() # OPTIONAL: render the whole scene + birds eye view
 
         player_action = human.action_step(obs['player_0']['pov'])
 
-        agent_actions = {'player_0': player_action}
+        if len(nextActs) > 0:
+            nextAct = nextActs.pop(0)
+        else:
+            if len(pathDict.keys())>0:
+                pass
+            else:
+                nextAct = 2
+        puppet_action = nextAct
+        agent_actions = {'player_0': player_action, 'player_1': puppet_action}
 
         next_obs, rew, done, info = env.step(agent_actions)
+
+        if info['player_1'] != {}:
+            if 'act' in info['player_1'].keys():
+                nextActs.append(info['player_1']['act'])
+            if 'path' in info['player_1'].keys():
+                pathDict = info['player_1']['path']
+
+        if pathDict != {}:
+            agent = env.instance_from_name['player_1']
+            sname = str(tuple(agent.pos))
+            if sname in pathDict.keys():
+                direction = pathDict[sname]
+            else:
+                direction = random.choice([0,1,2,3])
+            relative_dir = (agent.dir - direction) % 4
+            if relative_dir == 3 or relative_dir == 2:
+                nextActs.append(1)
+            elif relative_dir == 1:
+                nextActs.append(0)
+            elif relative_dir == 0:
+                nextActs.append(2)
         
         human.save_step(
             obs['player_0'], player_action, rew['player_0'], done
