7166f38cac76e2e2c054b66a768a2732d8ba5bc1
Joel Michelson <joelmichelson@users.noreply.github.com>
2022-04-09 14:16:08 -0500

    Update base_AEC.py

--- marlgrid/base_AEC.py
+++ marlgrid/base_AEC.py
@@ -440,19 +440,19 @@ class para_MultiGridEnv(ParallelEnv):
 
         # Gym spaces are defined and documented here: https://gym.openai.com/docs/#spaces
         #removed march
-        #self.action_spaces = {agent: Discrete(7) for agent in self.possible_agents}
+        self.action_spaces = {agent: Discrete(7) for agent in self.possible_agents}
         
         self.env_done = False
         self.step_count = 0
         #obshape = (45,45,3)
         
         #removed march
-        '''self.observation_spaces = {agent: Box(
+        self.observation_spaces = {agent: Box(
             low=0,
             high=255,
             shape=(self.agent_view_size, self.agent_view_size, 3),
             dtype='uint8'
-            ) for agent in self.possible_agents}'''
+            ) for agent in self.possible_agents}
 
         #self.action_space = self.action_spaces[self.possible_agents[0]]
         #self.observation_space = self.observation_spaces[self.possible_agents[0]]
