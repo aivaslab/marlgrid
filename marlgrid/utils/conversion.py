a8d237af5e0fb7174345923aaacdc7d57a6fd55d
Joel Michelson <joelmichelson@users.noreply.github.com>
2022-04-26 13:12:14 -0500

    Update conversion.py

--- marlgrid/utils/conversion.py
+++ marlgrid/utils/conversion.py
@@ -1,7 +1,7 @@
 from pettingzoo.utils.conversions import aec_to_parallel, parallel_to_aec
 import supersuit as ss
 from pettingzoo.utils import wrappers
-from.agents import GridAgentInterface
+from ..agents import GridAgentInterface
 
 def make_env(envClass, player_config, configName=None, memory=1, threads=1, reduce_color=False, size=64,
     reward_decay=False, ghost_mode=True, max_steps=50):
