e535736d964341923774cf43609414e470d5a55f
Joel Michelson <joelmichelson@users.noreply.github.com>
2022-04-28 15:48:47 -0500

    Update train.py

--- marlgrid/utils/train.py
+++ marlgrid/utils/train.py
@@ -39,7 +39,7 @@ def train_model(name, train_env, eval_envs, eval_params,
         if not os.path.exists(savePath):
             os.mkdir(savePath)
 
-    recordEvery = int(total_timesteps/evals)
+    recordEvery = int(total_timesteps/evals) if evals > 0 else 1000
     
     train_env = make_env(train_env[0], player_config, train_env[1], memory=memory, threads=threads,
                          reduce_color=reduce_color, size=size, reward_decay=reward_decay)
