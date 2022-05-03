a279fb5a3ce97cc0868d03bb93343f90f946f076
Joel Michelson <joelmichelson@users.noreply.github.com>
2022-05-03 14:51:16 -0500

    Update display.py

--- marlgrid/utils/display.py
+++ marlgrid/utils/display.py
@@ -62,9 +62,9 @@ def make_pic_video(model, env, name, savePics, saveVids, savePath, random_policy
         clip = moviepy.video.io.ImageSequenceClip.ImageSequenceClip(ims, 15)
         clip.write_videofile(os.path.join(savePath , vidname) + '.mp4')'''
 
-def plot_evals(name, stages, rewards, stds, history, savePath):
+def plot_evals(name, stages, rewards, stds, history, savePath, saveEvery=1):
     fig, axs = plt.subplots(1)
-    xaxis = range(len(rewards[stages[0]]))
+    xaxis = range(len(rewards[stages[0]]))*saveEvery
 
     for i, stage in enumerate(stages):
 
