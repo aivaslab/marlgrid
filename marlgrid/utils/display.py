76fccc5d773eff457cefd6f0ecc0240f860f000a
Joel Michelson <joelmichelson@users.noreply.github.com>
2022-05-08 11:12:59 -0500

    Update display.py

--- marlgrid/utils/display.py
+++ marlgrid/utils/display.py
@@ -7,6 +7,7 @@ import moviepy.video.io.ImageSequenceClip
 from pettingzoo.utils.conversions import aec_to_parallel, parallel_to_aec
 import datetime
 from stable_baselines3.common.results_plotter import load_results, ts2xy
+from stable_baselines3.common.monitor import get_monitor_files
 import numpy as np
 import pandas
 
